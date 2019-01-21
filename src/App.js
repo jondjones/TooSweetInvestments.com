4b6685ed5030ea40d4fab9e611196280c2e147b4
Jon <jon.jones.home@gmail.com>
2019-01-21 13:12:20 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import renderIf from 'render-if';
 
 class App extends Component {
   render() {
@@ -8,17 +9,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {renderIf(1 + 2 === 3)(
+            <span>The universe is working</span>
+          )}
         </header>
       </div>
     );
